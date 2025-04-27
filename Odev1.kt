a23a5ff12b7037f7e59d3635ea1793417f97e2e4
Yasemin Demir <yaaemin2002demir@gmail.com>
2025-04-27 16:00:53 +0300

    İlk Kotlin dosyası eklendi

---
+++ Odev1.kt
@@ -0,0 +1,122 @@
+package com.example.kotlinderslerim.degiskenler
+
+fun main() {
+    val sehir = "İstanbul"
+    var ulke = "Turkey"
+    var telefon = "5443391789"
+    var postaKodu = "66900"
+    var email = "yasemindemir.dev@gmail.com"
+    var meslek = "Computer Engineering Student"
+    var stokMiktarı = 6000
+    var musteriAdi = "Aleyna Cebeci"
+    var bakiye = 80000.50
+    var birthday = "20.01.2002"
+    var maas = 70000
+    var medeniDurum = "Bekar"
+    var urunYorum = "Ürün kullanışlı"
+
+    //PRINT PARTS
+    println("Şehir: $sehir ")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("Email: $email ")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktarı")
+    println("Müşterinin Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $birthday")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorumu: $urunYorum")
+
+
+
+    val odemeTarihi = "03.05.2026"
+    val odeme = "Kredi Kartı"
+    val siparisAdeti = 5
+    val arabaModel = "Mercedes Benz"
+    val kitapAdi = "Khaled Hosseini - Bin Muhteşem Güneş"
+    val yayinTarihi = "25.09.1956"
+    val indirimMiktari = 40
+    val odaSayisi = 100
+    val enlem = 41.0082
+    val boylam = 28.9784
+    val urunAdi = "Kalemlik"
+    val yemekFiyati =150.0
+
+    //PRINT PARTS
+    println("Ödeme Tarihi: $odemeTarihi ")
+    println("Ödeme: $odeme")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModel")
+    println("Kitap Adı: $kitapAdi ")
+    println("Yayın Tarihi: $yayinTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+
+
+    val marka = "Nike"
+    val muzikAdi = "Dedublüman - Belki"
+    val videoSuresi = "3:50"
+    val urunPuani = 2.5
+    val resimAdi = "colorful_things.png"
+    val dosyaFormati = "PDF"
+    val renk = "Kahverengi"
+    val renkKodu = "#FF0000"
+    val telefonModeli = "Galaxy A12"
+    val ekranBoyutu = 6.7
+    val agirlik = 180
+
+
+    //PRINT PARTS
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu inç")
+    println("Ağırlık: $agirlik gram")
+
+
+    val ulusalGun = "23 Nisan"
+    val tatilGunu = "19 Mayıs"
+    val rezervasyonTarihi = "10.09.2025"
+    val sokakAdi = "İpekyol Sokak"
+    val otobusHatti = "15B"
+    val kalanDakika = 13
+    val takipKodu = "TR123456789"
+    val kuponSuresi = "30 gün"
+    val kuponKodu = "YAZ25"
+    val faturaAdresi = "İstanbul, Kadıköy, Moda Mah. No:12"
+
+    //PRINT PARTS
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+
+    //örnek kısımlar kendimi sakinleştirmek  için ideal diye düşünüyorum
+
+    var yas = 23
+    var isim = "yasemin"
+    var soyad = "demir"
+    val sehira = "yozgat"
+    var bolum = "bilgisayar mühendisliği"
+
+    println("Kişinin yaşı: $yas ")
+}
